c6c72b5953ca6d4e918e6035d291357d23a8d19f
Evan White <kindasimple@gmail.com>
2017-12-28 14:11:20 -0500

    write output to console

--- index.ts
+++ index.ts
@@ -3,9 +3,14 @@ import { printSpiral } from './print'
 
 // handle parameters passed from commandline and output spiral
 if (require.main === module) {
-    const maxValue = parseInt(process.argv[2], 10) || 8
-    let spiral = generateSpiral(maxValue)
-    let digits = maxValue.toString().length + 1
-    let output = printSpiral(spiral, digits)
+    let input = process.argv[2]
+    if(input) {
+        const maxValue = parseInt(input, 10) || 8
+        let spiral = generateSpiral(maxValue)
+        let digits = maxValue.toString().length + 1
+        let output = printSpiral(spiral, digits)
+        console.log(output)
+    }
+
 }
 
